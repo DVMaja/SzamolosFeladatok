f86c8ac885d90bdd0ef12f170a79036371d8e4fa
DVMaja <majadreilinger@gmail.com>
2023-03-03 12:34:22 +0100

    2 sor szám kiírása

---
+++ SzamolosFeladatok/src/ciklusok/Szamok.java
@@ -0,0 +1,19 @@
+package ciklusok;
+
+public class Szamok {
+
+    public static void main(String[] args) {
+        //feladat: A 2,3,4 számjegyekből előállítható számok
+        //3 j3gyűek, de bármely számot bárhányszór szerepelhetnek
+        
+        
+        for (int j = 2; j <= 3; j++) {
+            for (int i = 2; i <= 4; i++) {
+                System.out.println("" + j + i);
+
+            }
+        }
+
+    }
+
+}
